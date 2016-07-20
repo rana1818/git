7e2a4112a92d4c3dad9d39f957d8f9265187dc4e
rana1818 <pranavreddy1818@gmail.com>
2016-07-20 11:22:34 -0500

    first

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,195 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Scorecard</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                        {{ csrf_field() }}
+
+                                                        
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+border: 1px solid black;
+
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+
+border: 1px solid #dddddd;
+    
+    padding:18px;
+   }
+</style>
+</head>
+ <form class="form-horizontal" role="form" method="POST" action="/admin/scoreedit/edit">
+
+<body>
+
+<div class="table-responsive">
+<table class="table table-bordered table-striped table-hover" >
+<tr>
+<th>players</th>
+
+    <th>Colours</th>
+    <th>Courseid</th>
+   <th>score1</th>
+ <th>score2</th>
+ <th>score3</th>
+ <th>score4</th>
+ <th>score5</th>
+ <th>score6</th>
+ <th>score7</th>
+ <th>score8</th>
+ <th>score9</th>
+ <th>score10</th>
+ <th>score11</th>
+ <th>score12</th>
+ <th>score13</th>
+ <th>score14</th>
+ <th>score15</th>
+ <th>score16</th>
+ <th>score17</th>
+ <th>score18</th>
+  <tr>
+ <td><div contenteditable> </div></td>
+    <td>                      </td>
+   <td>                        </td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+<td></td>
+ <td></td>
+ <td></td>
+
+  </tr>
+  <tr>
+   
+   <td></td>
+    <td></td>
+   <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+<td></td>
+ <td></td>
+ <td></td>
+
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+   <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+<td></td>
+ <td></td>
+ <td></td>
+
+  </tr>
+  <tr>
+   <td></td>
+    <td></td>
+   <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+<td></td>
+ <td></td>
+ <td></td>
+
+  </tr>
+  <tr>
+   <td></td>
+    <td></td>
+   <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+ <td></td>
+<td></td>
+ <td></td>
+ <td></td>
+ </tr>
+</table>
+</div>
+</body>
+</form>
+</html>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
